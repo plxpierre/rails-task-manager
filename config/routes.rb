77dd69edeb2b7b31e607850c6d322b2be449618d
Pierre Planex <pierre.planex@gmail.com>
2026-02-24 14:15:28 +0100

    rails task

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # read
+  get "tasks", to: "tasks#index"
+
+  # create
+  get "tasks/new", to: "tasks#new", as: :new_task
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
+
+  # edit
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # delete
+  delete "tasks/:id", to: "tasks#destroy"
 end
